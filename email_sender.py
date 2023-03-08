bddbd4c968de4c55ca42f8fab565f108f6eb1f10
Suraj Nag <suraj11nag@gmail.com>
2023-03-09 00:17:34 +0530

    first commit

---
+++ email_sender.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Suraj'
+email['to'] = 'suraj11nag@gmail.com'
+email['subject'] = 'Letter of acceptance to Hogwarts School of Witchcraft and Wizardry!'
+
+email.set_content(html.substitute({'name': 'Harry'}),'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls() #encryption mechanism, to connect securely to the server.
+    smtp.login('data11scientist@gmail.com', 'I_am_data_scientist@11')
+    smtp.send_message(email)
+    print('Email sent, boss!')
